7b76bca375cf8eb14c85ee9d127c26608e9183d8
rezawam <rotten.swamp@mail.ru>
2022-08-10 03:35:26 +0300

    added allowed hosts

--- FTch/settings.py
+++ FTch/settings.py
@@ -23,9 +23,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-0(ylnzyvfx&yoaorf2=#u2e1tocqot49v&9^s(l!b3zy4$90mc'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
