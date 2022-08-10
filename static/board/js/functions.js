3ca652b33bad99c9bdd7b681f8ae2fb9d00ff049
rezawam <rotten.swamp@mail.ru>
2022-08-10 13:47:40 +0300

    fuck my rot

---
+++ static/board/js/functions.js
@@ -0,0 +1,13 @@
+// function toggleForm() {
+//   let x = document.getElementById("input_form");
+//   if (x.style.display === "none") {
+//     x.style.display = "block";
+//   } else {
+//     x.style.display = "none";
+//   }
+// }
+
+// let message = document.getElementById('area');
+//
+// message.replace('бля', '<br/>');
+// console.log(message.value);
